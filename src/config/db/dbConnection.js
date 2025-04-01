396dd2875ddcf066ba89c17000b5f5b3634c0882
fathima riya <fathimariyaa096@gmail.com>
2025-04-01 14:17:18 +0530

    changed mongo connetion

--- src/config/db/dbConnection.js
+++ src/config/db/dbConnection.js
@@ -1,25 +1,25 @@
 const mongoose = require("mongoose");
-const env=require('dotenv')
-env.config()
-
+const env = require("dotenv");
+env.config();
 
 const connectDb = async () => {
-    
-    
-  await mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 30000,   // Increase timeout to 30 seconds
-    socketTimeoutMS: 45000             // Increase socket timeout
-  })
-    
-    .then(() => {
-      console.log("mongose connected");
-    })
-    .catch((err) => {
-      console.log(err);
+  console.log("MONGO_URL:", process.env.MONGO_URL); 
+  if (!process.env.MONGO_URL) {
+    throw new Error("MONGO_URL is not defined in environment variables");
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
     });
+    console.log("Mongoose connected");
+  } catch (err) {
+    console.error("Mongoose connection error:", err);
+    process.exit(1); 
+  }
 };
 
-
-module.exports=connectDb
+module.exports = connectDb;
